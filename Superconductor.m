317a1eb28a054f6ebdba323108702811c7d0993a
Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
2015-02-15 04:13:12 +0100

    work in progress, but Superconductor class is showing promise

--- Superconductor.m
+++ Superconductor.m
@@ -16,10 +16,10 @@ classdef Superconductor < handle
         gap         = [];
         states      = State.empty(0,0);
         
-        bc_left     = State.empty(0);
-        bc_right    = State.empty(0);
-        temperature = 1e-16;
-        scaling     = 1;
+        bc_left     = State.empty(0);       % Left boundary condition
+        bc_right    = State.empty(0);       % Right boundary condition
+        temperature = 1e-16;                % Temperature of the system
+        scaling     = 1;                    % Material property N₀λ
     end
     
     
@@ -123,45 +123,6 @@ classdef Superconductor < handle
         function boundary()
             % Calculates Kuprianov--Lukichev
         end
-        
-        
-        
-%         function gap = SuperconductingGap(states, energies, temperature, scaling)
-%     % This function takes a the singlet component of the Green's function
-%     % in a superconductor and the temperature of the system, and calculates
-%     % the superconducting gap of the material.
-%     %
-%     % Input:   
-%     %   states      This should be a vector containing either State objects
-%     %               (numerical solution of the Usadel eq) or complex numbers
-%     %               (the singlet components of the solutions).
-%     %   energies    This should be a vector of energies which corresponds
-%     %               to the states above.
-%     %   temperature The temperature of the material.
-%     %   cutoff      The Debye frequency cutoff of the material.
-%     %   scaling     The superconducting gap is proportional to this
-%     %               scaling constant (which is usually written N₀λ).
-%     % 
-%     % Output:  
-%     %   gap         The superconducting gap (in general a complex number)
-%     
-%     % Extract the singlet components from the states, if necessary
-%     singlets = zeros(1,numel(states));
-%     if isa(states, 'State')
-%         for n=1:length(singlets)
-%             singlets(n) = states(n).singlet;
-%         end
-%     else
-%         singlets = states;
-%     end
-% 
-%     % Create a cubic interpolation of the numerical data above, multiplied
-%     % by the tanh(ε/2T) kernel in the equation for the superconducting gap
-%     kernel = @(E) pchip(energies, singlets, E) .* tanh(E./(2*temperature));
-%     
-%     % Perform a numerical integration of the interpolation up to the cutoff
-%     gap    = scaling * integral(kernel, 0, energies(end));
-
     end
     
     
--- UsadelS.m
+++
@@ -1,1 +0,0 @@
-    xs = linspace(-1,0);%We want 100 solutions (sol) for different x between 0 and 1.
