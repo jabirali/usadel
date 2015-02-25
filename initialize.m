6a0089b9ba3d53d4da2bb26fbb800bcd8cf29417
Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
2015-02-25 22:37:32 +0100

    Reorganized the directory structure now that stuff works. Also decreased the infinitesimal triplet component in the initial state to h * 1e-100.

---
+++ initialize.m
@@ -0,0 +1,4 @@
+% This script initalizes the Matlab for using the 'bvp6c' differential equation solver and relevant Matlab classes by adding the relevant subdirectories to path. Run this before running any of the simulation scripts in this folder.
+
+addpath('BVP');
+addpath('Classes');
