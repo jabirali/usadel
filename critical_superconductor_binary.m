754f20894ab5311d9044d2f3a30982d68dfd1b8b
Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
2015-03-01 23:40:24 +0100

    Testing binary

--- critical_superconductor_binary.m
+++ critical_superconductor_binary.m
@@ -1,24 +1,34 @@
+% This script calculates the critical temperature of a bulk superconductor,
+% by performing a binary search for the temperature where the gap vanishes
+% numerically. The result should be numerically one in the given unit
+% system, so this script can be used to calibrate simulation parameters.
 
 
 
-% This script calculates the superconducting gap as a function of temperature.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                 DEFINE PARAMETERS FOR THE SIMULATION
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Define the physical parameters
-thouless     = 1/100;
-strength     = 0.2;
+strength     = 0.20;           % Material parameter N0V
+thouless     = 0.01;           % Thouless energy of the superconductor
+upper        = 1.50;           % Upper limit on the critical temperature
+lower        = 0.00;           % Lower limit on the critical temperature
 
 % Define the simulation parameters
 output       = 'output/critical_superconductor_binary/';
-positions    = linspace(0, 1, 100);
-temperatures = linspace(0, 1.5, 30);
+positions    = linspace(0, 1, 5);
 energies     = [linspace(0,0.500,100) linspace(0.501,1.500,100) linspace(1.501,cosh(1/strength),100)];
+iterations   = 8;
 
-% Create a superconductor based on the parameters above
-s = Superconductor(positions, energies, thouless, strength);
 
-% This variable is used to keep a backup of the last non-critical 
-% superconductor we had, which is used as an initial guess for the state
-sb = s.backup_save;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                   PREPARATIONS FOR THE SIMULATION
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Make sure that all required classes and methods are in the current path
+initialize;
 
 % Create the output directory (if it doesn't already exist)
 mkdir(output);
@@ -26,39 +36,79 @@ mkdir(output);
 % Log the program output to a file
 diary([output, 'log.txt']);
 
-% Define variables used to store results
-gap  = 1;                          % Current superconducting gap
-gaps = zeros(size(temperatures));  % All the calculated gaps
+% Create a superconductor object based on the parameters above
+s = Superconductor(positions, energies, thouless, strength);
+
+% Make sure that all debugging options are disabled
+s.delay = 0;
+s.debug = 0;
+s.plot  = 0;
+
+% This variable is used to keep a backup of the last non-critical object
+sb = s.backup_save;
+
+% Define variables used to store the tested temperatures and gaps
+temperatures = [lower upper]; % Which temperatures we have tested
+gaps         = [1 0];         % Which superconducting gaps we have found
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%          PERFORM A BINARY SEARCH FOR THE CRITICAL TEMPERATURE
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 tic;
-for n=1:length(temperatures)    
-    % Status information
-    fprintf('\n:: PROGRAM:        [ %3d / %3d ]  [ Time: %2d min ]\n                   T = %.6f, gap = %.6f\n\n',  n, length(temperatures), floor(toc/60), temperatures(n), gap);
-
-    % Increase the superconductor temperature, and update the gap and state
-    s.temperature = temperatures(n);
-    s.update_gap;
-    s.update;
+for n=1:iterations
+    % Restore the current superconductor state from backup
+    s.backup_load(sb);
     
-    % Keep updating the internal state of the superconductor until the gap
-    % converges (i.e. less than 1% change between iterations)
+    % Set the current temperature to the average of the two previous values
+    s.temperature = (upper+lower)/2;
+    
+    % Status information
+    fprintf(':: PROGRAM: [ %3d / %3d ] [ T = %.6f ] [ Time: %2d min ]\n',  n, iterations, s.temperature, floor(toc/60));
+    
+    % Update the internal state of the superconductor until the mean gap
+    % converges, i.e. the value stabilizes within 1% fluctuations.
+    gap = inf;
     while (abs(1 - s.mean_gap/gap) > 1e-2) && ~s.critical
-        % Status information
-        fprintf('\n:: PROGRAM:        [ %3d / %3d ]  [ Time: %2d min ]\n                   T = %.6f, gap = %.6f\n                   Gap changed by %.2f%%. Recalculating...\n\n',  n, length(temperatures), floor(toc/60), temperatures(n), s.mean_gap, 100*abs(1-s.mean_gap/gap));
-            
         % Update the superconductor state
         gap = s.mean_gap;
         s.update;
+
+        % Status information
+        fprintf(':: PROGRAM: [ %3d / %3d ] [ Temp: %.6f ] [ Time: %2d min ] [ Gap: %.6f ]\n            Gap changed by %.2f%%.\n',  n, iterations, s.temperature, floor(toc/60), s.mean_gap, 100*abs(1-s.mean_gap/gap));            
     end
     
-    % Store the current result in the vector 'gaps'
-    gaps(n) = s.mean_gap;
+    % Store the current temperature and gap as results
+    temperatures(end) = s.temperature;
+    gaps(end)         = s.mean_gap;
+    
+    % If the system has gone critical, update the upper estimate for the
+    % critical temperature. If not, update the lower one, and use the
+    % current state as an initial guess from now on.
+    if s.critical
+        upper = current;
+    else
+        lower = current;
+        sb    = s.backup_save;
+    end
 end
 
-% Save the results to file
-save([output, 'output.mat'], 'temperatures', 'gaps');
 
-% Disable the logging
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%           SAVE RESULTS AND CLEAN UP AFTER THE SIMULATION
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% The final estimate of the critical temperature is the mean of the current
+% upper and lower limits obtained by the above calculations
+critical_temperature = (upper+lower)/2;
+
+% Save the results to file
+save([output, 'results.mat'], 'critical_temperature', 'temperatures', 'gaps');
+
+% Disable the log from now
 diary off;
 
 % Plot the results
--- critical_superconductor_linear.m
+++ critical_superconductor_linear.m
@@ -14,6 +14,9 @@ energies     = [linspace(0,0.500,100) linspace(0.501,1.500,100), linspace(1.501,
 s = Superconductor(positions, energies, thouless, strength);
 s.debug = false;
 
+% Make sure that all required classes and methods are in the current path
+initialize;
+
 % Create the output directory (if it doesn't already exist)
 mkdir(output);
 
@@ -35,10 +38,10 @@ for n=1:length(temperatures)
     s.update;
     
     % Keep updating the internal state of the superconductor until the gap
-    % converges (i.e. less than 1% change between iterations)
-    while (abs(1 - s.mean_gap/gap) > 1e-2) && ~s.critical
+    % converges (i.e. less than 0.1% change between iterations)
+    while (abs(gap - s.mean_gap) > 1e-3) && (abs(gap) > 1e-3)
         % Status information
-        fprintf('\n:: PROGRAM: [ %3d / %3d ] [ Time: %2d min ] [ T = %.6f ] [ gap = %.6f ]\n            Gap changed by %.2f%%. Recalculating...\n\n',  n, length(temperatures), floor(toc/60), temperatures(n), s.mean_gap, 100*abs(1-s.mean_gap/gap));
+        fprintf('\n:: PROGRAM: [ %3d / %3d ] [ Time: %2d min ] [ T = %.6f ] [ gap = %.6f ]\n            Gap changed by %.2f%%. Recalculating...\n',  n, length(temperatures), floor(toc/60), temperatures(n), s.mean_gap, 100*abs(1-s.mean_gap/gap));
             
         % Update the superconductor state
         gap = s.mean_gap;
@@ -50,7 +53,7 @@ for n=1:length(temperatures)
 end
 
 % Save the results to file
-save([output, 'output.mat'], 'temperatures', 'gaps');
+save([output, 'results.mat'], 'temperatures', 'gaps');
 
 % Disable the logging
 diary off;
