0d809132366ca4c0bd1e48df68bc47b97a0bc29a
Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
2015-03-07 15:08:31 +0100

    Added two new job scripts

---
+++ Launchers/partasks_ds_hx_a0.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+# PBS job script for launching the 'critical_bilayer' simulation.
+#
+# Written by Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
+# Based on an example script by Roy Dragseth and Egil Holsvik.
+
+##################################################################
+# Configure the PBS queue system
+##################################################################
+#PBS -M jabirali@switzerlandmail.ch
+#PBS -A acc-fysikk
+#PBS -q default	
+#PBS -lnodes=1:ppn=12
+#PBS -lwalltime=150:00:00
+#PBS -lpmem=2000MB
+
+maxtasks=12
+
+##################################################################
+# Set the simulation parameters
+##################################################################
+commands=( 'critical_bilayer(0.4, 0.5, 0.2, [ 10 0 0 ], 0, pi/4)'
+           'critical_bilayer(0.5, 0.5, 0.2, [ 10 0 0 ], 0, pi/4)'
+           'critical_bilayer(0.6, 0.5, 0.2, [ 10 0 0 ], 0, pi/4)'
+           'critical_bilayer(0.7, 0.5, 0.2, [ 10 0 0 ], 0, pi/4)'
+           'critical_bilayer(0.8, 0.5, 0.2, [ 10 0 0 ], 0, pi/4)'
+           'critical_bilayer(0.9, 0.5, 0.2, [ 10 0 0 ], 0, pi/4)'
+           'critical_bilayer(0.4, 1.0, 0.2, [ 10 0 0 ], 0, pi/4)'
+           'critical_bilayer(0.5, 1.0, 0.2, [ 10 0 0 ], 0, pi/4)'
+           'critical_bilayer(0.6, 1.0, 0.2, [ 10 0 0 ], 0, pi/4)'
+           'critical_bilayer(0.7, 1.0, 0.2, [ 10 0 0 ], 0, pi/4)'
+           'critical_bilayer(0.8, 1.0, 0.2, [ 10 0 0 ], 0, pi/4)'
+           'critical_bilayer(0.9, 1.0, 0.2, [ 10 0 0 ], 0, pi/4)' )
+
+##################################################################
+# Perform the simulation
+##################################################################
+
+# Launch the simulation script once for each parameter defined above
+module load matlab
+for n in $(seq 0 9); do
+	# Make a work directory for Matlab
+    	workdir="/work/$LOGNAME/matlab/ds_hx_a0/${n}"
+    	mkdir -p "$workdir"
+    	cd "$workdir"
+
+    	# Link the required scripts into the work directory
+    	ln -st . "$PBS_O_WORKDIR/BVP" "$PBS_O_WORKDIR/Classes" "$PBS_O_WORKDIR/initialize.m" "$PBS_O_WORKDIR/critical_bilayer.m"
+
+    	# Execute the simulation script in the background
+    	echo $(uname -n) $(date +'%H:%M:%S') "Starting simulation:"
+	echo "::  ${commands[n]}"
+    	{
+		echo "${commands[n]}";
+    		matlab -nodisplay -r "${commands[n]}";
+    	} 1>output.log 2>error.log &
+
+    	# Wait until activetasks < maxtasks before continuing the loop
+    	activetasks=$(jobs | wc -l)
+    	while [ $activetasks -ge $maxtasks ]; do
+    		sleep 5
+    		activetasks=$(jobs | wc -l)
+    	done
+done
+module unload matlab
+
+# Wait for all tasks to finish
+echo "Waiting for tasks to complete"
+wait
+echo "All jobs complete!"
